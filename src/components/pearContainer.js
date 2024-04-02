a6749f741bfe4d8d2ccf265915eeff4922399737
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-02 07:04:48 -0400

    add pear container for buying pears

---
+++ src/components/pearContainer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { buyPear } from "../redux";
+import { connect } from "react-redux";
+
+const PearContainer = (props) => {
+  return (
+    <div>
+      <h2>Number of Pears - {props.numOfPears}</h2>
+      <button onClick={props.buyPear}>Buy Pear</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    numOfPears: state.pear.numOfPears,
+  };
+};
+
+const mapDispatchToProps = (state) => {
+  return {
+    buyPear: () => dispatch(buyPear()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PearContainer);

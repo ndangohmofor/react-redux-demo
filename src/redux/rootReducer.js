a84d7914abb9d11eb1a51bada71faeb49c9182e5
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-02 06:59:21 -0400

    add the pear reducer in the root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import icecreamReducer from "./icecream/icecreamReducer";
 import cakeReducer from "./cake/cakeReducer";
+import pearReducer from "./pear/pearReducer";
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
   icecream: icecreamReducer,
+  pear: pearReducer,
 });
 
 export default rootReducer;

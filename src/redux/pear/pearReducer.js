3ccbec0aa355004ae4f3adae28d726199992efbd
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-02 06:56:43 -0400

    add pear reducer

---
+++ src/redux/pear/pearReducer.js
@@ -0,0 +1,18 @@
+import { BUY_PEAR } from "./pearTypes";
+
+const initialState = {
+  numOfPears: 50,
+};
+
+const pearReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_PEAR:
+      return {
+        ...state,
+        numOfPears: state.numOfPears - 1,
+      };
+
+    default:
+      return state;
+  }
+};

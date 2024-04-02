eb1ac12521ac71052aaff9b61683974cbfc0faf0
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-01 23:12:57 -0400

    import the new store into the app js file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import HookscakeContainer from "./components/HookscakeContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HookscakeContainer />
         <CakeContainer />
       </div>
     </Provider>

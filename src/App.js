fb24aad6be7449feb38f950264e0c2876fc0af40
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-03 05:50:44 -0400

    add the all new containers into app js file

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ import HookscakeContainer from "./components/HookscakeContainer";
 import IcecreamContainer from "./components/icecreamContainer";
 import PearContainer from "./components/pearContainer";
 import HooksPearContainer from "./components/HooksPearContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <HookscakeContainer />
         <CakeContainer />
         <IcecreamContainer />
         <PearContainer />
         <HooksPearContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );

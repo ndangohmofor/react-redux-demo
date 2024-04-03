4813e56d7d43e8baa02d1348b8c9cea123deb835
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-03 06:13:08 -0400

    add userReducer into the combined reducers

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import icecreamReducer from "./icecream/icecreamReducer";
 import cakeReducer from "./cake/cakeReducer";
 import pearReducer from "./pear/pearReducer";
+import userReducer from "./user/userReducer";
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
   icecream: icecreamReducer,
   pear: pearReducer,
+  user: userReducer,
 });
 
 export default rootReducer;

dadf0176acdcb7d7904585505c01a7c6b7d44355
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-03 06:25:29 -0400

    export fetchUsers function

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -25,7 +25,7 @@ export const fetchUserFailure = (error) => {
   };
 };
 
-const fetchUsers = () => {
+export const fetchUsers = () => {
   return (dispatch) => {
     dispatch(fetchUserRequest);
     axios
